90c7d81bd9becf737a3831563ccc9326defe14ae
bookdeno <bookdeno@example.com>
2026-01-22 22:40:02 -0800

    Fix URL paths for custom permission views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import add_book, edit_book, delete_book
+from . import views
 
 urlpatterns = [
-    path('books/add/', add_book, name='add_book'),
-    path('books/<int:book_id>/edit/', edit_book, name='edit_book'),
-    path('books/<int:book_id>/delete/', delete_book, name='delete_book'),
+    path("add_book/", views.add_book, name="add_book"),
+    path("edit_book/<int:book_id>/", views.edit_book, name="edit_book"),
+    path("delete_book/<int:book_id>/", views.delete_book, name="delete_book"),
 ]
 
