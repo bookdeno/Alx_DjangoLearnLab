a01a346713e98a8d19107d2939f7ae915aac9c21
bookdeno <bookdeno@example.com>
2026-02-08 22:16:09 -0800

    Add Django function-based and class-based views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from . import views
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
-    path("add_book/", views.add_book, name="add_book"),
-    path("edit_book/<int:book_id>/", views.edit_book, name="edit_book"),
-    path("delete_book/<int:book_id>/", views.delete_book, name="delete_book"),
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
-
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,32 +1,16 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.decorators import permission_required
-from .models import Book
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+from .models import Book, Library
 
-@permission_required('relationship_app.can_add_book', raise_exception=True)
-def add_book(request):
-    if request.method == 'POST':
-        title = request.POST.get('title')
-        author_id = request.POST.get('author')
-        publication_year = request.POST.get('publication_year')
-        Book.objects.create(title=title, author_id=author_id, publication_year=publication_year)
-        return redirect('list_books')
-    return render(request, 'relationship_app/add_book.html')
 
-@permission_required('relationship_app.can_change_book', raise_exception=True)
-def edit_book(request, book_id):
-    book = get_object_or_404(Book, pk=book_id)
-    if request.method == 'POST':
-        book.title = request.POST.get('title')
-        book.publication_year = request.POST.get('publication_year')
-        book.save()
-        return redirect('list_books')
-    return render(request, 'relationship_app/edit_book.html', {'book': book})
+# Function-based view: list all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
 
-@permission_required('relationship_app.can_delete_book', raise_exception=True)
-def delete_book(request, book_id):
-    book = get_object_or_404(Book, pk=book_id)
-    if request.method == 'POST':
-        book.delete()
-        return redirect('list_books')
-    return render(request, 'relationship_app/delete_book.html', {'book': book})
 
+# Class-based view: library detail
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
